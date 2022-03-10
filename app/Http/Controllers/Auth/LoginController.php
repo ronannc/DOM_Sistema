b5700033e7483b6a3132522f611657fc8acf555d
ronan <ronannc1@yahoo.com.br>
2022-03-09 23:07:48 -0300

    add procfile for heroku

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,4 +36,9 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function welcome()
+    {
+        return view('welcome');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'auth/LoginController@welcome')->name('home');
+
 
 Auth::routes();
 
